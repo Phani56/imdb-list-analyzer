08189338c833d062df82e78f8dc845ecf0239ee5
Tomi Kasurinen <tomi.kasurinen@gmail.com>
2018-08-27 14:45:43 +0300

    Updated Clojure, ClojureScript and lein-cljsbuild versions.

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "http://github.com/dresa/imdb-list-analyzer"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [cheshire "5.5.0"]
-                 [org.clojure/clojurescript "1.7.170"]
+                 [org.clojure/clojurescript "1.10.339"]
                  [reagent "0.5.1"]
                  [figwheel-sidecar "0.5.0-SNAPSHOT" :scope "provided"]
                  [compojure "1.4.0"]
@@ -14,26 +14,26 @@
                  [cljs-ajax "0.5.1"]]
 
   :plugins       [[lein-ring "0.9.7"]
-                  [lein-cljsbuild "1.1.1"]]
+                  [lein-cljsbuild "1.1.7"]]
 
   :ring {:handler imdb-list-analyzer.server/app}
 
   :profiles {:production
              {:hooks [leiningen.cljsbuild]
               :cljsbuild {
-                         :builds [{
-                                   :source-paths ["src"]
-                                   :compiler {
-                                              :id "prod"
-                                              :output-to "resources/public/js/main.js"  ; default: target/cljsbuild-main.js
-                                              :output-dir "resources/public/js"
-                                              :figwheel false
-                                              :optimizations :whitespace
-                                              :pretty-print false}}]}
-             :ring {
-                    :open-browser? false
-                    :stacktraces? false
-                    :auto-reload? false}}}
+                          :builds [{
+                                    :source-paths ["src"]
+                                    :compiler {
+                                               :id "prod"
+                                               :output-to "resources/public/js/main.js"  ; default: target/cljsbuild-main.js
+                                               :output-dir "resources/public/js"
+                                               :figwheel false
+                                               :optimizations :whitespace
+                                               :pretty-print false}}]}
+              :ring {
+                     :open-browser? false
+                     :stacktraces? false
+                     :auto-reload? false}}}
 
   :min-lein-version "2.0.0"
   :main imdb-list-analyzer.core)
